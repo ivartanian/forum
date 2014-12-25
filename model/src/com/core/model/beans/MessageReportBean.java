a7d51c5bf81e94eef637303bac4642c779c78d43
ivartanian <igor.vartanian@gmail.com>
2014-12-25 14:23:19 +0200

    OK

---
+++ model/src/com/core/model/beans/MessageReportBean.java
@@ -0,0 +1,31 @@
+package com.core.model.beans;
+
+import javax.ejb.ActivationConfigProperty;
+import javax.ejb.MessageDriven;
+import javax.jms.JMSException;
+import javax.jms.Message;
+import javax.jms.MessageListener;
+import javax.jms.TextMessage;
+
+/**
+ * Created by i.vartanian on 25.12.2014.
+ */
+@MessageDriven(name = "MessageReportEJB", mappedName = "jms.ForumTopic",
+        activationConfig = {
+                @ActivationConfigProperty(propertyName = "connectionFactoryJndiName", propertyValue = "jms.TopicFactory"),
+                @ActivationConfigProperty(propertyName = "destinationName", propertyValue = "jms.ForumTopic"),
+                @ActivationConfigProperty(propertyName = "destinationType", propertyValue = "javax.jms.Topic")
+        })
+public class MessageReportBean implements MessageListener {
+    public MessageReportBean() {
+    }
+
+    @Override
+    public void onMessage(Message message) {
+        try {
+            System.out.println("URA!!!!!!!!!!!!!!!!!!!!" + ((TextMessage) message).getText());
+        } catch (JMSException e) {
+            e.printStackTrace();
+        }
+    }
+}
